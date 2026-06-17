0acb0a3ecf56cd5a3bcc5fe7258b6a0205fa1332
Vanir <valentin.eder2034@gmail.com>
2026-06-17 09:25:18 +0200

    Bump mod version to 1.3.0 for Server List feature

--- shapez-multiplayer.js
+++ shapez-multiplayer.js
@@ -1,7 +1,7 @@
 const METADATA = {
   id: "multiplayer",
   name: "Shapez.io Multiplayer",
-  version: "1.2.0",
+  version: "1.3.0",
   description: "Real-time cooperative multiplayer for shapez.io",
   author: "AI",
   website: "",
